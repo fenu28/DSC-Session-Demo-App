7115219b6da92e47198f2cf8a2d990ed38d37070
fenu28 <kamdarfenil88@gmail.com>
2021-03-19 14:42:47 +0530

    Optimized imports and commented code

--- app/src/main/java/com/fenil/demoapp/LoginActivity.java
+++ app/src/main/java/com/fenil/demoapp/LoginActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Declaring the variables that link with the user interface components
     EditText username;
     EditText password;
     Button login;
@@ -18,25 +19,30 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login); //Links the UI of LoginActivity to resource file activity_login.xml
 
+        //findViewById() function is used to find components in resource file. We pass the integer ID of component as argument
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         login = findViewById(R.id.login);
 
+        //setOnClickListener() function is used to implement actions that we need to perform when user clicks on a component like button, text view etc.
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                //We extract the string value of data which the user gave as input in username and password fields.
                 String inputUsername = username.getText().toString();
                 String inputPassword = password.getText().toString();
 
+                //If either of username and password field is empty, we display a toast message to fill all values before proceeding to login
                 if(inputUsername.isEmpty() || inputPassword.isEmpty())
                 {
                     Toast.makeText(LoginActivity.this,"Please fill all the values",Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
+                    //This is an example of implicit intent. We use implicit intents to navigate between different activities
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                 }
--- app/src/main/java/com/fenil/demoapp/MainActivity.java
+++ app/src/main/java/com/fenil/demoapp/MainActivity.java
@@ -1,12 +1,6 @@
 package com.fenil.demoapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -16,21 +10,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class MainActivity extends AppCompatActivity {
 
+    //This variable is a request code to get calling permission from Android system.
     private static final int CALL_PERMISSION_CODE = 1;
+
+    //Declaring the variables that link with the user interface components
     Button youtube;
     Button instagram;
     Button whatsapp;
     Button twitter;
     Button call;
     EditText phoneNumber;
+
+    //We store the input phone number provided by user in this variable
     String number;
+
     PackageManager packageManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Links the UI of MainActivity to resource file activity_main.xml
 
         youtube = findViewById(R.id.youtube);
         instagram = findViewById(R.id.instagram);
@@ -40,16 +46,33 @@ public class MainActivity extends AppCompatActivity {
         phoneNumber = findViewById(R.id.phone_number);
         packageManager = getApplicationContext().getPackageManager();
 
+        // Attaching an onClickListener to Youtube button for handling touch/click events
         youtube.setOnClickListener(new View.OnClickListener() {
             Intent youtubeIntent;
             @Override
             public void onClick(View v) {
+
+                /* We are using a try-catch block here. A try-catch block helps in execution of code that may produce error.
+                * If errors are not handled, our app may crash.
+                * Here in try block, we first check if the app user is trying to open is installed on the phone.
+                * If the target app is not installed, a NullPointerException occurs. We handle this exception in the catch block*/
+
                 try{
+                    /* We pass the package name of target app as an argument of getLaunchIntentForPackage().
+                    * If the app is installed, getLaunchIntentForPackage() will return a non-null intent.
+                    * We store the returned intent in a local variable and pass it to startActivity() as argument.
+                    * By calling startActivity(), the target app will be started through intent without any errors.*/
                     youtubeIntent = packageManager.getLaunchIntentForPackage("com.google.android.youtube");
                     startActivity(youtubeIntent);
                 }
                 catch (NullPointerException e)
                 {
+                    /* In the catch block, we are going to handle scenario where target app is not installed on the phone.
+                    * We are going to show the target app's respective website to the user in a browser.
+                    * First we parse the URL of the website as a Uri. We call Uri.parse() method and pass the string URL as argument
+                    * We set the mode of our intent as Intent.ACTION_VIEW. This mode is used when we want to show user some data outside of our app.
+                    * When user clicks on the button, Android will show a list of compatible apps that can display the data we parsed as Uri.
+                    * As we are giving argument a website URL, Android will display a list of browsers.*/
                     youtubeIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.youtube.com"));
                     startActivity(youtubeIntent);
                 }
@@ -57,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Attaching an onClickListener to Instagram button for handling touch/click events
         instagram.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Attaching an onClickListener to Whatsapp button for handling touch/click events
         whatsapp.setOnClickListener(new View.OnClickListener() {
             Intent whatsappIntent;
             @Override
@@ -90,6 +115,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        // Attaching an onClickListener to Twitter button for handling touch/click events
         twitter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,17 +134,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Attaching an onClickListener to Call button for handling touch/click events
         call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Stores the input phone number obtained from EditText.
                 number = phoneNumber.getText().toString();
+
+                //Condition to check if any input was provided
                 if(number.isEmpty())
                 {
+                    //Display toast asking user to enter phone number before calling.
                     phoneNumber.setError("Please enter phone number");
                     phoneNumber.requestFocus();
                 }
                 else
                 {
+                    /* Case where user has provided input\
+                    * The calling permission is a dangerous permission.
+                    * Dangerous permissions need consent of user at runtime.
+                    * Here we check if calling permission is granted or not by the user*/
                     checkPermission(Manifest.permission.CALL_PHONE, CALL_PERMISSION_CODE);
                 }
             }
@@ -143,10 +180,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
                 Toast.makeText(MainActivity.this, "Call Permission Granted", Toast.LENGTH_SHORT).show();
+
+                // Intent.ACTION_CALL indicates calling mode of the intent
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
+
+                //We parse the phone number in a format recognized by calling intent
                 callIntent.setData(Uri.parse("tel:" + number));
                 startActivity(callIntent);
             }
